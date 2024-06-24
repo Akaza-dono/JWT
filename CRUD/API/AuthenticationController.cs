f8ae0eaf93ccf4a1dbb24b4336824e2a4296ef86
akaza-dono <alejoquevedo0598@hotmail.com>
2024-06-23 23:09:17 -0500

    Autenticacion con JWT y entity framework

---
+++ CRUD/API/AuthenticationController.cs
@@ -0,0 +1,54 @@
+﻿using CRUD.Data;
+using CRUD.DDBBModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Runtime.InteropServices;
+using System.Security.Claims;
+using System.Text;
+
+namespace CRUD.API
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly string _authenticationToken;
+
+        public AuthenticationController(IConfiguration config)
+        {
+            var apiTokenSection = config.GetSection("Setting").GetSection("Apitoken").ToString();
+            _authenticationToken = apiTokenSection;
+        }
+
+        [HttpPost("Validar")]
+        public IActionResult Validar([FromBody] Usuario user)
+        {
+            using(CRUDbContext context = new())
+            {
+                var usuario = context.Usuarios.Where(User => User.Correo == user.Correo).FirstOrDefault();
+                if (usuario != null && usuario.Password == user.Password) 
+                {
+                    var keyBytes = Encoding.ASCII.GetBytes(_authenticationToken);
+                    var claims = new ClaimsIdentity();
+                    claims.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Correo ?? string.Empty));
+
+                    var tokenDescriptor = new SecurityTokenDescriptor
+                    {
+                        Subject = claims,
+                        Expires = DateTime.UtcNow.AddMinutes(5),
+                        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(keyBytes), SecurityAlgorithms.HmacSha256Signature)
+                    };
+                    var tokenHandler = new JwtSecurityTokenHandler();
+                    var tokenConfig = tokenHandler.CreateToken(tokenDescriptor);
+
+                    string token = tokenHandler.WriteToken(tokenConfig);
+
+                    return StatusCode(StatusCodes.Status200OK, new { token = token });
+                }
+            }
+            return StatusCode(StatusCodes.Status401Unauthorized, new { token = "" });
+        }
+    }
+}
---
+++ CRUD/Program.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("appsettings.json");
+var secretKey = builder.Configuration.GetSection("Setting").GetSection("Apitoken").ToString();
+
+var keyBytes = secretKey != null ? Encoding.UTF8.GetBytes(secretKey) : Encoding.UTF8.GetBytes("");
+
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(config =>
+{
+    config.RequireHttpsMetadata = false;
+    config.SaveToken = true;
+    config.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
+        ValidateIssuerSigningKey = false,
+        ValidateAudience = false
+    };
+});
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
